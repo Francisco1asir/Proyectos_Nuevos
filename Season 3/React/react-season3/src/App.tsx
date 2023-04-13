68652dbd883e24d4572c38dbf2c5f21c06744eee
Francisco1asir <fparnav1908@g.educaand.es>
2023-04-13 10:28:54 +0200

    push

---
+++ Season 3/React/react-season3/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { NavBar } from './common/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import { Main } from './components/Main';
+import { routes } from './common/routes';
+function App() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+        {
+          routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component/>}
+            >
+              {route.name}
+            </Route>
+          ))
+        }
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
