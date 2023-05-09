bd16b698ab933c604589529277cdd81dfaca5089
Francisco1asir <fparnav1908@g.educaand.es>
2023-05-09 12:38:11 +0200

    AñadirDatosFB

--- Season 3/React/react-season3/src/firebase/FBcategorias.ts
+++ Season 3/React/react-season3/src/firebase/FBcategorias.ts
@@ -1,12 +1,27 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, setDoc, doc } from "firebase/firestore";
 import { useFirestore } from "reactfire";
 import { firebaseConfig } from "./config/firebaseConfig";
 import { ICategoria } from "../interfaces/ICategoria";
+import { nanoid } from 'nanoid'
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore() //use database para las credenciales
 
+//AÑADIR CATEGORIAS A FB
+export const newCategoria = async (data: ICategoria) => {
+    try{
+        console.log('Insertando en FB el objeto', data)
+        const newData = {codigo: nanoid(20), ...data}
+        const docRef = doc(db, "Categorias", newData.codigo);
+        await setDoc(docRef, newData);
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+//LISTADO DE CATEGORIAS DE FB
 export const getCategorias = async ():Promise<ICategoria[]> => {
     let categorias: ICategoria[] = []; //array inicializado al vacio
     // const categoriasRef = collection(useFirestore(), "Categorias") //seleccionamos la coleccion categorias
@@ -19,3 +34,4 @@ export const getCategorias = async ():Promise<ICategoria[]> => {
     console.log(categorias);
     return categorias;
 }
+
--- Season 3/React/react-season3/src/pages/CategoriasPage.tsx
+++ Season 3/React/react-season3/src/pages/CategoriasPage.tsx
@@ -1,20 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { getCategorias } from '../firebase/FBcategorias';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import { getCategorias, newCategoria } from '../firebase/FBcategorias';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import { ICategoria } from '../interfaces/ICategoria';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Backdrop from '@mui/material/Backdrop';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
 import { TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
 
 export const CategoriasPage = () => {
   // const [categorias, setCategorias] = useState([{ name: '' }])
@@ -32,39 +23,60 @@ export const CategoriasPage = () => {
       })
   }, [])
 
-  getCategorias()
+  const { register, handleSubmit } = useForm<ICategoria>();
+  // getCategorias()
 
-    return (
-      <>
-        <Grid container sx={{ display: 'flex', justifyContent: 'space-around' }}>
-          <Grid item xs={5} sx={{ backgroundColor: 'lightgray', margin: '10px', padding: '12px', height: '28vh', borderRadius: '20px' }}>
-            <h2 id='NewCat'>Listado de Categorias</h2>
-            {
-              categorias.map((categoria) => (
-                <>
-                  <li key={categoria.name}>{categoria.name}</li>
-                </>
-              ))
-            }
-          </Grid>
-          <Grid item xs={5} sx={{ backgroundColor: 'lightgray', margin: '10px', padding: '12px', height: '28vh', borderRadius: '20px' }}>
-            <h2 id='NewCat'>Añadir nueva Categoría</h2>
-            <form action="">
-                <TextField 
-                // { ...register('name')}
-                id='nombre'
-                label='Nombre'
-                type='text'
-                sx={{width: '50%'}}
-                >
-                </TextField>             
-            </form>
-            <Button variant="contained" sx={{marginTop: '10px'}}>Añadir Producto</Button>
-          </Grid>
-        </Grid>
+  // const onAddCategoria = ( dataCategoria: ICategoria) => {
+  //   console.log(dataCategoria)
+  //   newCategoria(dataCategoria)
+  // }
 
-
-      </>
-    )
+  const onAddCategoria = async ( dataCategoria: ICategoria) => {
+    // console.log('Enviando...')
+    console.log(dataCategoria)
+    await newCategoria(dataCategoria)
+    window.location.reload();
   }
-  export default CategoriasPage;
+
+  return (
+    <>
+      <Grid container sx={{ display: 'flex', justifyContent: 'space-around' }}>
+        <Grid item xs={5} sx={{ backgroundColor: 'lightgray', margin: '10px', padding: '12px',  height: 'max-content', borderRadius: '20px' }}>
+          <h2 id='NewCat'>Listado de Categorias</h2>
+          {
+            categorias.map((categoria) => (
+              <>
+                <li key={categoria.name}>{categoria.name}</li>
+              </>
+            ))
+          }
+        </Grid>
+        <Grid item xs={5} sx={{ backgroundColor: 'lightgray', margin: '10px', padding: '12px', height: 'max-content', borderRadius: '20px' }}>
+          <h2 id='NewCat'>Añadir nueva Categoría</h2>
+          <form onSubmit={handleSubmit(onAddCategoria)} noValidate >
+            <TextField
+              {...register('name')}
+              id='nombre'
+              label='Nombre'
+              type='text'
+              sx={{ width: '50%' }}
+            >
+            </TextField>
+
+            <TextField
+              {...register('logo')}
+              label='Logo'
+              type='string'
+              sx={{ width: '30%' }}
+            >
+            </TextField>
+            <Button type='submit' variant="contained" sx={{ marginTop: '10px' }}>Añadir Producto</Button>
+          </form>
+        </Grid>
+      </Grid>
+
+
+    </>
+  )
+}
+export default CategoriasPage;
